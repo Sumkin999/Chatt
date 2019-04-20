1d78a9e680d91b86ea39e92297205607ead07663
Fedor <sumkin6969@gmail.com>
2019-04-20 12:28:52 +0300

    Window stuff

--- src/Client/VlientWindowAgain.java
+++ src/Client/VlientWindowAgain.java
@@ -16,6 +16,7 @@ public class VlientWindowAgain {
 
 	private JFrame frame;
 	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -52,21 +53,36 @@ public class VlientWindowAgain {
 		frame.setTitle("Chat Window");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		//frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		
 		//textField = new JTextField();
 		//frame.getContentPane().add(textField, BorderLayout.NORTH);
+		
+		frame.getContentPane().add(textField, BorderLayout.NORTH);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
 		TextArea textArea= new TextArea();
 		textArea.setEditable(false);
-		frame.getContentPane().add(textField, BorderLayout.NORTH);
+		//textArea.setEditable(false);
 		
 		JScrollPane scrollPane = new JScrollPane(textArea);
-		frame.getContentPane().add(scrollPane);
+		frame.getContentPane().add(scrollPane,BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		textField_1 = new JTextField();
+		panel_1.add(textField_1);
+		textField_1.setColumns(40);
+		
+		JButton btnNewButton = new JButton("Send");
+		panel_1.add(btnNewButton);
 		
 		
 		textField.setColumns(10);
